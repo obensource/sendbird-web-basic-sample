784c8fb2f6d35a055c6c8f29816ef348c0f3c149
Ben Michel <ben@obensource.com>
2020-08-27 09:14:14 -0700

    more dd RUM finaggling

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
 import { isEmpty, setCookie, getCookie } from './utils';
 import { USER_ID, KEY_ENTER } from './const';
-import { Datacenter, datadogRum } from '@datadog/browser-rum';
-
-datadogRum.init({
-    applicationId: 'dc6509e5-98bf-465c-a300-9527336d17c9',
-    clientToken: 'pubbc27149433c49e8b9fb34fd70c4d3667',
-    site: 'datadoghq.com',
-    service: 'sendboard-app',
-    env: 'heroku-demo',
-    sampleRate: 100,
-    trackInteractions:true,
-});
 
 const userIdEl = document.querySelector('#user_id');
 const nicknameEl = document.querySelector('#user_nickname');

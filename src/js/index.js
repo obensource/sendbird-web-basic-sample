b40b2db882823167f7d98a9068de67be014b309b
Ben Michel <ben@obensource.com>
2020-08-27 15:09:32 -0700

    use browser js

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,24 @@
 import { isEmpty, setCookie, getCookie } from './utils';
 import { USER_ID, KEY_ENTER } from './const';
-import { datadogRum } from '@datadog/browser-rum';
 
 const userIdEl = document.querySelector('#user_id');
 const nicknameEl = document.querySelector('#user_nickname');
 const buttonEl = document.querySelector('#login-button');
 
-datadogRum.init({
-    applicationId: 'dc6509e5-98bf-465c-a300-9527336d17c9',
-    clientToken: 'pubbc27149433c49e8b9fb34fd70c4d3667',
-    site: 'https://datadoghq.com',
-    sampleRate: 100,
-    trackInteractions: true
+window.DD_RUM && window.DD_RUM.init({
+  applicationId: 'dc6509e5-98bf-465c-a300-9527336d17c9',
+  clientToken: 'pubbc27149433c49e8b9fb34fd70c4d3667',
+  site: 'https://datadoghq.com',
+  sampleRate: 100,
+  trackInteractions: true
 });
 
-datadogRum.addRumGlobalContext('usr', {
+window.DD_RUM && window.DD_RUM.addRumGlobalContext('usr', {
   id: `USER-${USER_ID}`,
   plan: 'demo-plan'
 });
 
-datadogRum.setRumGlobalContext({
+window.DD_RUM && window.DD_RUM.setRumGlobalContext({
   demo: 'Admin Message Performance'
 });
 

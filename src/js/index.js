5394f0707063ac23720b77fdd88ead37a3db8715
Ben Michel <ben@obensource.com>
2020-08-27 14:54:52 -0700

    try secure dd address

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ const buttonEl = document.querySelector('#login-button');
 datadogRum.init({
     applicationId: 'dc6509e5-98bf-465c-a300-9527336d17c9',
     clientToken: 'pubbc27149433c49e8b9fb34fd70c4d3667',
-    site: 'datadoghq.com',
+    site: 'https://datadoghq.com',
     sampleRate: 100,
     trackInteractions: true
 });

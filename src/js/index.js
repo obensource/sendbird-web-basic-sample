7e4786b4d851c8ef6d8becfe2c6faa53b1d392db
Ben Michel <ben@obensource.com>
2020-08-27 08:11:39 -0700

    add DD RUM agent

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
 import { isEmpty, setCookie, getCookie } from './utils';
 import { USER_ID, KEY_ENTER } from './const';
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: '88233088ae6f8a0726da6266c8b39acfc7ef979e',
+    clientToken: '6422e1ad74893350bd20a288e24600c2',
+    site: 'datadoghq.com',
+    service: 'sendboard-app',
+    env: 'heroku-demo',
+    sampleRate: 100,
+    trackInteractions:true,
+});
 
 const userIdEl = document.querySelector('#user_id');
 const nicknameEl = document.querySelector('#user_nickname');
